4d685d262e94936dad067fbe1fdaebccea1570e1
wutong <1135465070@qq.com>
2022-03-27 17:21:18 +0800

    更改layout

--- src/layout/Index.vue
+++ src/layout/Index.vue
@@ -1,27 +1,53 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+const useNativeScrollbar = false;
+</script>
 <template>
-  <n-layout>
-    <n-layout-sider>sider</n-layout-sider>
-    <n-layout>
-      <n-layout-header>header</n-layout-header>
-      <n-layout-content>content</n-layout-content>
-      <n-layout-footer>footer</n-layout-footer>
-    </n-layout>
+  <!-- <n-layout-header bordered>header </n-layout-header> -->
+  <n-layout has-sider class="main-layout">
+    <n-layout-sider bordered show-trigger
+      >sider
+      <div>6666dsds</div>
+      <div>6666dsds</div>
+      <div>6666dsds</div>
+      <div>6666dsds</div>
+      <div>6666dsds</div>
+    </n-layout-sider>
+    <n-layout-content
+      >content
+      <div>6666dsds</div>
+      <div>6666dsds</div>
+      <div>6666dsds</div>
+      <div>6666dsds</div>
+      <div>6666dsds</div>
+    </n-layout-content>
+    <n-layout-footer bordered position="absolute">footer</n-layout-footer>
   </n-layout>
 </template>
 
 <style scoped>
+.main-layout {
+  position: absolute;
+  left: 0;
+  right: 0;
+  top: 0;
+  bottom: 0;
+}
+.n-layout-header {
+  height: 45px;
+}
+.n-layout-footer {
+  height: 35px;
+}
 .n-layout-header,
 .n-layout-footer {
-  background: rgba(128, 128, 128, 0.2);
-  padding: 24px;
+  /* background-color: gray; */
 }
 
 .n-layout-sider {
-  background: rgba(128, 128, 128, 0.3);
+  /* background: rgba(128, 128, 128, 0.3); */
 }
 
 .n-layout-content {
-  background: rgba(128, 128, 128, 0.4);
+  /* background: rgba(128, 128, 128, 0.4); */
 }
 </style>
